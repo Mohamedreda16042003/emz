da200a082a381ad224c06e2c8dd0f943ccc41643
Mohamedreda16042003 <mohamedreda16042003@gmail.com>
2023-07-19 14:07:04 +0300

    comment

---
+++ js/data.js
@@ -0,0 +1,206 @@
+export const data = [
+  {
+    name: "Lily Rose",
+    comment:
+      "EMZ is very responsive and very much willing to continue working until the design was to my desired outcome.",
+  },
+  {
+    name: "John Paul",
+    comment:
+      "Made this process super easy and was patient. Will work again with him in the near future.",
+  },
+  {
+    name: "Mary Jane",
+    comment: "Perfect, quality, quick, and professional!",
+  },
+  {
+    name: "Sarah Elizabeth",
+    comment: "Excellent, thanks!!!!",
+  },
+  {
+    name: "James Michael",
+    comment: "Perfect and super fast work, thank you!",
+  },
+  {
+    name: "Emma Grace",
+    comment:
+      "By far, the best Pro Seller I ever worked with. He's proactive and provides you with more than what you thought needed. A great asset and definitely you can 100% rely on.",
+  },
+  {
+    name: "William Thomas",
+    comment:
+      "I had the pleasure of working with EMZ on a new website project for my business, and I couldn't be happier with the results. From the initial consultation to the final launch, he was professional, communicative, and incredibly talented. He took the time to really understand my business and my goals for the website and worked closely with me to develop a custom design that perfectly captured my brand's personality and message. Throughout the process, he provided regular updates and was always available to answer any questions or address any concerns I had. I'm very happy with this experience, will continue to use his services, and highly to anyone in need!",
+  },
+  {
+    name: "Olivia Marie",
+    comment:
+      "Great to work with! Patient, knowledgeable, confident and a super nice! It was a pleasure! Thanks so much!",
+  },
+  {
+    name: "Robert Edward",
+    comment: "Great communication and deliveries!",
+  },
+  {
+    name: "Ava Rose",
+    comment:
+      "Super responsive and responsible. Very trustworthy. Totally recommend it",
+  },
+  {
+    name: "David Anthony",
+    comment: "Very good, good communication, does what he promised",
+  },
+  {
+    name: "Sophia Grace",
+    comment: "Great work!",
+  },
+  {
+    name: "Christopher David",
+    comment: "amazing work",
+  },
+  {
+    name: "Madison Paige",
+    comment: "competent and supportive. affordable price.",
+  },
+  {
+    name: "Benjamin James",
+    comment: "Extremely professional, always helpful and available! Thank you!",
+  },
+  {
+    name: "Chloe Anne",
+    comment:
+      "you did an amazing job. You were so thorough and patient through this whole process. I would be back.",
+  },
+  {
+    name: "Samuel Joseph",
+    comment:
+      "Went above and beyond my expectations. Will definitely be coming back in the future. Highly recommend it to anyone. Very sleek and modern design. And affordable as well.",
+  },
+  {
+    name: "Emily Kate",
+    comment: "Very fast and efficient!",
+  },
+  {
+    name: "Nicholas William",
+    comment:
+      "Excellent and well performed with a clear understanding about the subject and requirements",
+  },
+  {
+    name: "Isabella Rose",
+    comment:
+      "Excellent developer who takes time to explain the improvements he has made",
+  },
+  {
+    name: "Alexander John",
+    comment:
+      "Insanely fast and willing to do every single changes that I requested, thank you very much",
+  },
+  {
+    name: "Victoria Anne",
+    comment: "Fast and talented designer! Thank you",
+  },
+  {
+    name: "Matthew Robert",
+    comment:
+      "Everything is working now and I will keep working with them on future projects.",
+  },
+  {
+    name: "Grace Elizabeth",
+    comment: "Simply amazing. Highly skilled and professional.",
+  },
+  {
+    name: "Jonathan David",
+    comment: "Perfect job",
+  },
+  {
+    name: "Abigail Grace",
+    comment: "Great website and easy to work with. Very fast response time",
+  },
+  {
+    name: "Daniel Joseph",
+    comment: "Fast service good price",
+  },
+  {
+    name: "Caroline Elizabeth",
+    comment: "Quick response! Easy to communicate. Quality work!",
+  },
+  {
+    name: "Patrick Michael",
+    comment:
+      "The design is exactly what I was looking for, and he was great at communicating throughout the process. Highly recommend!",
+  },
+  {
+    name: "Julia Marie",
+    comment: "Great value, great communication. Provided as promised.",
+  },
+  {
+    name: "Andrew Thomas",
+    comment: "Outstanding and highly professional.",
+  },
+  {
+    name: "Ella Rose",
+    comment: "Fantastic to work with!",
+  },
+  {
+    name: "Timothy James",
+    comment: "Very easy and professional experience with EMZ.",
+  },
+  {
+    name: "Charlotte Grace",
+    comment: "I love the website at the end, nice touch.",
+  },
+  {
+    name: "Stephen Michael",
+    comment: "Great communication & delivery work",
+  },
+  {
+    name: "Samantha Marie",
+    comment: "Prompt and professional! We are happy with the output.",
+  },
+  {
+    name: "Joshua David",
+    comment:
+      "I have no words to describe the satisfaction I got, did way more than I paid for. I suggest it to everyone.",
+  },
+  {
+    name: "Leah Marie",
+    comment: "The communication was beyond awesome. Highly recommended.",
+  },
+  {
+    name: "Ryan Patrick",
+    comment:
+      "great, patient, helpful, kind, and professional. Delivery was super fast and quality was above expectations. Would recommend!! Thanks, EMZ",
+  },
+  {
+    name: "Harper Grace",
+    comment:
+      "Absolutely great to work with! Will definitely use again, very efficient with time and delivered exactly as described!",
+  },
+  {
+    name: "Kevin Michael",
+    comment: "Very smooth.",
+  },
+  {
+    name: "Penelope Rose",
+    comment: "Highly recommended.",
+  },
+  {
+    name: "Peter Thomas",
+    comment:
+      "Great Communication, Very Professional Work and work ethics. I Love my new Website. the greatest!",
+  },
+  {
+    name: "Addison Marie",
+    comment:
+      "Great website designer. good and quick communication. Choose EMZ, you won't be disappointed.",
+  },
+  {
+    name: "Josephine Rose",
+    comment:
+      "Fast and reliable service that was delivered in time - will go back and can only recommend it!!",
+  },
+  {
+    name: "Caleb James",
+    comment:
+      "Great experience!  was knowledgeable, and communicative, and exceeded our expectations.",
+  },
+];
--- js/index.js
+++ js/index.js
@@ -4,21 +4,21 @@ const btnMenu = document.querySelector(".menu");
 const linkes = document.querySelectorAll(".link_nav");
 
 window.addEventListener("scroll", () => {
-    if (window.scrollY >= 60) {
-        navScroll.classList.add("on_nav-active");
-    } else if (window.scrollY < 60) {
-        navScroll.classList.remove("on_nav-active");
-    }
+  if (window.scrollY >= 60) {
+    navScroll.classList.add("on_nav-active");
+  } else if (window.scrollY < 60) {
+    navScroll.classList.remove("on_nav-active");
+  }
 });
 
 linkes.forEach((ele) => {
-    ele.addEventListener("click", () => {
-        listNav.classList.remove("activer_menu-nav");
-    });
+  ele.addEventListener("click", () => {
+    listNav.classList.remove("activer_menu-nav");
+  });
 });
 
 btnMenu.addEventListener("click", () => {
-    listNav.classList.toggle("activer_menu-nav");
+  listNav.classList.toggle("activer_menu-nav");
 });
 // ...........................................
 
@@ -26,52 +26,70 @@ const commentBtn = document.getElementById("comment_btn");
 const nameComment = document.getElementById("name_area");
 const textComment = document.getElementById("comment_area");
 const apComment = document.getElementById("all_comment");
+const seeMore = document.getElementById("see_more");
 
-commentBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    post();
-    setTimeout(() => {
-        window.location.reload();
-    }, 1500);
+// commentBtn.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   //   post();
+//   setTimeout(() => {
+//     window.location.reload();
+//   }, 1500);
+// });
+
+// function post() {
+//     axios
+//         .post("http://localhost:1337/api/comments", {
+//             data: {
+//                 Name: nameComment.value,
+//                 Comment: textComment.value,
+//             },
+//         })
+//         .then(function (response) {
+//             console.log(response);
+//         })
+//         .catch(function (error) {
+//             console.log(error);
+//         });
+// }
+
+// async function getComment() {
+//     const res = await axios.get("http://localhost:1337/api/comments");
+
+//     addToComment(res.data.data);
+// }
+
+//^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-
+
+import { data } from "./data.js";
+
+//^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-
+
+let numComment = 5;
+
+seeMore.addEventListener("click", () => {
+  numComment += 5;
+  addToComment(data);
 });
 
-function post() {
-    axios
-        .post("http://localhost:1337/api/comments", {
-            data: {
-                Name: nameComment.value,
-                Comment: textComment.value,
-            },
-        })
-        .then(function (response) {
-            console.log(response);
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-}
-
-async function getComment() {
-    const res = await axios.get("http://localhost:1337/api/comments");
-
-    addToComment(res.data.data);
-}
+addToComment(data);
 
 function addToComment(arr) {
-    let comment = "";
-    for (let i = 0; i < arr.length; i++) {
-        comment += `
+  let comment = "";
+  for (let i = 0; i < numComment; i++) {
+    comment += `
         <div class="comment_card">
-            <div class="div_img"><img src="./img/user-removebg-preview.png" alt=""></div>
+            <div class="div_img">
+                <img src="./img/user-removebg-preview.png" alt="">
+                <h4>${arr[i].name}</h4>
+            </div>
             <div>
-                <h4>${arr[i].attributes.Name}</h4>
-                <p>${arr[i].attributes.Comment}</p>
+                <p>${arr[i].comment}</p>
             </div>
         </div>
         `;
-    }
-    console.log(textComment);
-    apComment.innerHTML = comment;
+  }
+
+  apComment.innerHTML = comment;
 }
 
-getComment();
+// getComment();
